2eb31824a7e5ba2f927d6f8ed82d2cace50553c3
Dash Desai <1723932+iamontheinet@users.noreply.github.com>
2022-07-15 15:08:27 -0700

    Initial commit

---
+++ do_something_cool.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+def greetings(name):
+  greet = f"Hello, {name}! How are you doing today?"
+  return greet
+
+if __name__ == "__main__":
+  name = "Cool Cat"
+  if len(sys.argv) > 1:
+      name = sys.argv[1] 
+  greetings(name)
