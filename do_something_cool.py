0098eca58b6bf0399829db817becabbca063d8dc
Dash Desai <iamontheinet@gmail.com>
2022-07-15 18:20:34 -0700

    Updated code

--- do_something_cool.py
+++ do_something_cool.py
@@ -3,7 +3,7 @@
 import sys
 
 def greetings(name):
-  greet = f"Hello, {name}! How are you today my friend?"
+  greet = f"Hello, {name}! How are you doing my friend?"
   return greet
 
 if __name__ == "__main__":
